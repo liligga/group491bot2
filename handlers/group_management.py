73453d9fafc26bb511e392089795f5f5fb05291f
Igor <igglike@gmail.com>
2025-01-29 16:04:31 +0300

    Добавил фильрт плохих слов и команду для бана других пользователей

--- handlers/group_management.py
+++ handlers/group_management.py
@@ -5,9 +5,23 @@ from aiogram.filters import Command
 group_router = Router()
 group_router.message.filter(F.chat.type != 'private')
 
-@group_router.message()
+BAD_WORDS = ("дурак", "тупой", "изгой")
+
+
+@group_router.message(Command("ban", prefix="/!"))
+async def ban_user_handler(message: types.Message):
+    print(message.reply_to_message)
+    if message.reply_to_message:
+        author_id = message.reply_to_message.from_user.id
+        await message.chat.ban(author_id)
+
+
+@group_router.message(F.text)
 async def echo_handler(message: types.Message):
-    print(message.chat.type)
-    # print(message.chat)
-    await message.answer("Привет")
+    for word in BAD_WORDS:
+        print(word)
+        if word in message.text:
+            await message.answer("Такое слово нельзя использовать")
+            await message.delete()
+    
 
